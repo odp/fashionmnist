93066f2e19295c738e893827f2bfc544738661b4
Omkar Pangarkar <opangarkar3@gatech.edu>
2018-05-28 14:04:25 -0400

    add local.sh

---
+++ local.sh
@@ -0,0 +1,8 @@
+python simple.py --ps_hosts=localhost:2222,localhost:2223 --worker_hosts=localhost:2224,localhost:2225 --job_name=ps --task_index=0 &
+
+python simple.py --ps_hosts=localhost:2222,localhost:2223 --worker_hosts=localhost:2224,localhost:2225 --job_name=ps --task_index=1 &
+
+python simple.py --ps_hosts=localhost:2222,localhost:2223 --worker_hosts=localhost:2224,localhost:2225 --job_name=worker --task_index=0 &
+
+python simple.py --ps_hosts=localhost:2222,localhost:2223 --worker_hosts=localhost:2224,localhost:2225 --job_name=worker --task_index=1 &
+
